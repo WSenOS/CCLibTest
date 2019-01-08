ece76796029cf2355e50edbca1bfeee3d6076a57
wangsen <wangsenwd@hotmail.com>
2019-01-08 09:26:39 +0800

    init

---
+++ CCLibTest/Classes/CCTest.h
@@ -0,0 +1,19 @@
+//
+//  CCTest.h
+//  VanthinkDemo
+//
+//  Created by sen on 2019/1/8.
+//  Copyright © 2019 sen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCTest : NSObject
+
++ (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCLibTest/Classes/CCTest.m
@@ -0,0 +1,17 @@
+//
+//  CCTest.m
+//  VanthinkDemo
+//
+//  Created by sen on 2019/1/8.
+//  Copyright © 2019 sen. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
++ (void)test {
+    NSLog(@"私有库测试");
+}
+
+@end
